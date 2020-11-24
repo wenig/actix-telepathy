f5088b8c5a46e8a69db395f579edbd88aa7ea3ee
wenig <info@pwenig.de>
2020-11-24 17:51:44 +0100

    fix: fabfile

--- start_basic.sh
+++ start_basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for DECENTFL_PROCESS in $(seq 0 `expr $1 - 1`); do
+for DECENTFL_PROCESS in $(seq 0 9); do
   args="${@:2}"
-  echo "RUST_ENV=debug cargo run --package decentfl --bin basic" #$(eval "echo $args") &
+  echo "RUST_ENV=debug cargo run --package decentfl --bin basic" $(eval "echo $args") &
 done
 wait
