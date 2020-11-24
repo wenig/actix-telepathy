349f36be9a0e97450abbe0cc49dd16cfe5267ecb
wenig <info@pwenig.de>
2020-11-24 18:18:03 +0100

    fix: fabfile

--- start_basic.sh
+++ start_basic.sh
@@ -3,6 +3,6 @@
 source .env
 
 for DECENTFL_PROCESS in $(seq 0 `expr $DECENTFL_PROCESSES - 1`); do
-  RUST_ENV=debug cargo run --package decentfl --bin basic $HOSTNAME:`expr $DECENTFL_BASEPORT + $DECENTFL_PROCESS` $DECENTFL_BASEHOST:$DECENTFL_BASEPORT &
+  RUST_ENV=debug cargo run --package basic --bin basic $HOSTNAME:`expr $DECENTFL_BASEPORT + $DECENTFL_PROCESS` $DECENTFL_BASEHOST:$DECENTFL_BASEPORT &
 done
 wait
