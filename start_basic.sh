f0d310e5bf7421be62aeb67c2020747e585cf871
wenig <info@pwenig.de>
2020-11-24 18:34:00 +0100

    fix: fabfile

--- start_basic.sh
+++ start_basic.sh
@@ -7,6 +7,6 @@ for DECENTFL_PROCESS in $(seq 0 `expr $DECENTFL_PROCESSES - 1`); do
   if [[ "$DECENTFL_BASEHOST:$DECENTFL_BASEPORT" != "$host" ]]; then
     server=$DECENTFL_BASEHOST:$DECENTFL_BASEPORT
   fi
-  RUST_ENV=debug cargo run --package basic --bin basic $host $server &
+  RUST_LOG=debug cargo run --package basic --bin basic $host $server &
 done
 wait
