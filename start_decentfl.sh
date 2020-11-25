9379e59192cda7e800610fec9312ad8d2e54cab5
wenig <info@pwenig.de>
2020-11-25 14:52:31 +0100

    fix: set num threads before training starts

--- start_decentfl.sh
+++ start_decentfl.sh
@@ -2,13 +2,16 @@
 
 source .env
 
+splitoff=0
+
 for DECENTFL_PROCESS in $(seq 0 `expr $DECENTFL_PROCESSES - 1`); do
   host=$HOSTNAME:`expr $DECENTFL_BASEPORT + $DECENTFL_PROCESS`
   if [[ "$DECENTFL_BASEHOST:$DECENTFL_BASEPORT" != "$host" ]]; then
     seed="--seed-nodes $DECENTFL_BASEHOST:$DECENTFL_BASEPORT"
+    splitoff=`expr $splitoff + 1`
   fi
   server=$DECENTFL_BASEHOST:$DECENTFL_BASEPORT
-  split=`expr $DECENTFL_SPLIT_OFFSET + $DECENTFL_PROCESS`
+  split=`expr $DECENTFL_SPLIT_OFFSET + $DECENTFL_PROCESS - $splitoff`
   args="${@:1}"
   RUST_LOG=info cargo run --package decentfl --bin decentfl $host $args --db-path "decentfl.$split.db" --split $split --server-addr $server $seed &
 done
