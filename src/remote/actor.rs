00e546dca7afb82d0b5d510d8dae526638902c66
wenig <info@pwenig.de>
2021-04-07 19:07:07 +0200

    refactor: RemoteActor must be Actor, RemoteMessage must be Message + Send + Serialize

--- src/remote/actor.rs
+++ src/remote/actor.rs
@@ -1,7 +1,10 @@
 use crate::{AddrResolver, AddrRequest, RemoteWrapper};
-use actix::{SystemService, Recipient};
+use actix::{SystemService, Recipient, Actor};
 
-pub trait RemoteActor {
+pub trait RemoteActor
+where
+    Self: Actor
+{
     fn register(&mut self, rec: Recipient<RemoteWrapper>, identifier: String) {
         AddrResolver::from_registry().do_send(
             AddrRequest::Register(rec, identifier)
