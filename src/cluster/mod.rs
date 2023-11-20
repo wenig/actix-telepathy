ba76219759f927a098afa17b9211fe9c2ed78627
wenig <phillip.wenig@hpi.de>
2023-11-20 09:07:59 +0100

    chores: fmt

--- src/cluster/mod.rs
+++ src/cluster/mod.rs
@@ -54,12 +54,9 @@ pub enum NodeEvent {
 impl Display for NodeEvent {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            NodeEvent::MemberUp(node, seed) => write!(
-                f,
-                "MemberUp: {} (seed: {})",
-                node.socket_addr,
-                seed
-            ),
+            NodeEvent::MemberUp(node, seed) => {
+                write!(f, "MemberUp: {} (seed: {})", node.socket_addr, seed)
+            }
             NodeEvent::MemberDown(addr) => write!(f, "MemberDown: {}", addr),
         }
     }
